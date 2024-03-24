e86aa0c6f75742da54728ff522c6a18cb54b80f5
leinadDebug <akujuaobidaniel@gmail.com>
2024-03-24 12:45:33 +0100

    -modified * added StarRating.jsx * added bk icon btn * resolved the filterdata err

--- src/components/viewMovie.jsx
+++ src/components/viewMovie.jsx
@@ -1,14 +1,24 @@
-import { data } from "autoprefixer";
+import { FiArrowLeftCircle } from "react-icons/fi";
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import StarRating from "../components/StarRating";
 
 export default function viewMovie({ movies }) {
   const [filteredMovie, setFilteredMovie] = useState("");
   const { parms_Id } = useParams("/movies/");
 
-  const filteredData = movies.filter((m) => m.Title === parms_Id);
+  const filteredData = movies.filter((m) => m.Title || m.title === parms_Id);
   console.log(filteredData);
   const [{ Title }] = filteredData;
 
-  return <div>{Title}</div>;
+  return (
+    <div className="m-2 p-3 pb-5 flex flex-col gap-4">
+      <button className=" mr-auto">
+        <FiArrowLeftCircle size={28} color="gold" />
+      </button>
+      <h1>{Title}</h1>
+
+      <StarRating maxRate={10} color="gold" size="20" />
+    </div>
+  );
 }

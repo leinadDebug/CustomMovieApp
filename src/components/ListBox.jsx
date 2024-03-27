97e2bd8c625b22f42a2c620f0e3f6db29647092e
leinadDebug <akujuaobidaniel@gmail.com>
2024-03-27 14:46:09 +0100

    --mod * imported custom scroll <webkit /index.css --------------------- * added custom scroll scroll

--- src/components/ListBox.jsx
+++ src/components/ListBox.jsx
@@ -2,10 +2,7 @@ import React, { useState } from "react";
 import { Movies } from "./constants";
 import { useNavigate } from "react-router-dom";
 
-
-
 export default function ListBox({ movies }) {
-  
   const [isClicked, setClicked] = useState("false");
 
   const handleClick = () => {
@@ -13,18 +10,25 @@ export default function ListBox({ movies }) {
   };
 
   return (
-    <div className={`bg-[#0a1c24] flex-1 rounded-xl text-white`}>
-      <div className="flex justify-end m-2">
+    <div
+      className={`bg-[#0a1c24] flex-1 flex rounded-xl text-white max-h-[80vh]`}
+    >
+      <div className="justify-end m-2 fixed ">
         <button
           className="rounded-full bg-[#050c0f] text-white m-3 px-2"
-          onClick={handleClick}>
+          onClick={handleClick}
+        >
           {isClicked ? "-" : "+"}
         </button>
       </div>
-      {movies.map(
-        (movie, index) =>
-          isClicked && <Movies key={index} className="text-white" movie={movie} ></Movies>
-      )}
+      <div className="my-2 mt-20 overflow-y-auto scrollbar-thin scrollbar-webkit">
+        {movies.map(
+          (movie, index) =>
+            isClicked && (
+              <Movies key={index} className="text-white" movie={movie}></Movies>
+            )
+        )}
+      </div>
     </div>
   );
 }
